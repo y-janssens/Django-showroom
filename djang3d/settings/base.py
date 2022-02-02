6f31ef8a7636f86f3d0d276bc59efa2a0d7fa932
Geoffrey Royer <geoffrey.royer@gmail.com>
2022-02-02 01:15:20 +0100

    Set modularized settings

--- djang3d/settings/base.py
+++ djang3d/settings/base.py
@@ -77,15 +77,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djang3d.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
